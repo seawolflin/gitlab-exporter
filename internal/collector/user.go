f6ba1ebcd3c3a6a60efaeb8b3081178034a108ce
seawolflin <linjianhai1989@live.cn>
2022-04-02 16:29:33 +0800

    feat: 实现用户相关的指标

--- internal/collector/user.go
+++ internal/collector/user.go
@@ -2,12 +2,17 @@ package collector
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/seawolflin/gitlab-exporter/internal/common/utils"
 	gitlabUser "github.com/seawolflin/gitlab-exporter/internal/gitlab/user"
 	"strconv"
+	"time"
 )
 
 type userCollector struct {
-	userStateMetric *prometheus.Desc
+	userStateMetric          *prometheus.Desc
+	userCreateAtMetric       *prometheus.Desc
+	userIsAdminMetric        *prometheus.Desc
+	userLastActivityOnMetric *prometheus.Desc
 }
 
 func init() {
@@ -18,12 +23,21 @@ func init() {
 func newUserCollector() *userCollector {
 	return &userCollector{
 		userStateMetric: prometheus.NewDesc("gitlab_user_state",
-			"", []string{"name", "userName", "createAt", "isAdmin", "lastActivityOn"}, nil),
+			"用户状态，1 - active, 2 - blocked, -1 - 未知", []string{"id", "name", "userName", "email"}, nil),
+		userCreateAtMetric: prometheus.NewDesc("gitlab_user_create_at",
+			"用户创建时间，单位：s", []string{"id", "name", "userName", "email"}, nil),
+		userIsAdminMetric: prometheus.NewDesc("gitlab_user_is_admin",
+			"用户是否是管理员", []string{"id", "name", "userName", "email"}, nil),
+		userLastActivityOnMetric: prometheus.NewDesc("gitlab_user_last_activity_on",
+			"用户活跃时间，单位：s", []string{"id", "name", "userName", "email"}, nil),
 	}
 }
 
 func (collector *userCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.userStateMetric
+	ch <- collector.userCreateAtMetric
+	ch <- collector.userIsAdminMetric
+	ch <- collector.userLastActivityOnMetric
 }
 
 func (collector *userCollector) Collect(ch chan<- prometheus.Metric) {
@@ -32,13 +46,20 @@ func (collector *userCollector) Collect(ch chan<- prometheus.Metric) {
 	for _, user := range users {
 		ch <- prometheus.MustNewConstMetric(collector.userStateMetric, prometheus.GaugeValue,
 			convertStateToValue(user.State),
-			user.Name, user.Username, user.CreatedAt.String(), strconv.FormatBool(user.IsAdmin),
-			func() string {
-				if user.LastActivityOn != nil {
-					return user.LastActivityOn.String()
-				}
-				return ""
-			}())
+			strconv.Itoa(user.ID), user.Name, user.Username, user.Email)
+		ch <- prometheus.MustNewConstMetric(collector.userCreateAtMetric, prometheus.GaugeValue,
+			float64(user.CreatedAt.Unix()),
+			strconv.Itoa(user.ID), user.Name, user.Username, user.Email)
+		ch <- prometheus.MustNewConstMetric(collector.userIsAdminMetric, prometheus.GaugeValue,
+			utils.ConvertBoolToValue(user.IsAdmin),
+			strconv.Itoa(user.ID), user.Name, user.Username, user.Email)
+
+		if user.LastActivityOn != nil {
+			lastActivityOn := time.Time(*user.LastActivityOn)
+			ch <- prometheus.MustNewConstMetric(collector.userLastActivityOnMetric, prometheus.GaugeValue,
+				float64(lastActivityOn.Unix()),
+				strconv.Itoa(user.ID), user.Name, user.Username, user.Email)
+		}
 	}
 }
 
